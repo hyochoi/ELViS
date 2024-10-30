5b1d55995c6147687f38e271f6cc7d157674dc15
Jay Lee <jlee307@login2.cn.isaac.utk.edu>
2024-10-29 20:13:11 -0400

    maketxdbfromgff3 fix

--- R/Plotting.R
+++ R/Plotting.R
@@ -5,8 +5,7 @@ utils::globalVariables(c(".", "%>%"))
 
 #' @import ggplot2
 #' @import rmarkdown
-#' @import txdbmaker
-#' @importFrom GenomicFeatures transcriptsBy cdsBy genes
+#' @importFrom GenomicFeatures transcriptsBy cdsBy genes makeTxDbFromGFF
 #' @rawNamespace import(GenomicRanges, except=c(subtract))
 #' @noRd
 get_gene_anno_plot_ori <- function(
@@ -25,7 +24,7 @@ get_gene_anno_plot_ori <- function(
   arrow_spacing_bp <- as.integer(arrow_spacing*space_length)
 
   # prep annotation
-  txdb <- makeTxDbFromGFF(gff3_fn, format = "gff3")
+  txdb <- GenomicFeatures::makeTxDbFromGFF(gff3_fn, format = "gff3")
   all_features <- transcriptsBy(txdb, by = "gene")
   cds <- cdsBy(txdb, by = "gene")
   if(!is.null(exclude_genes)){
@@ -912,8 +911,8 @@ get_gene_rnt_ori <- function(
     ...
 ){
 
+    txdb <- GenomicFeatures::makeTxDbFromGFF(gff3_fn, format = "gff3")
 
-  txdb <- makeTxDbFromGFF(gff3_fn, format = "gff3")
 
   genes <- genes(txdb) %>% sort
   cds <- cdsBy(txdb, by = "gene")
@@ -1055,7 +1054,7 @@ gene_cn_heatmaps <-
     baseline_target <- baseline
 
 
-    txdb <- makeTxDbFromGFF(gff3_fn, format = "gff3")
+    txdb <- GenomicFeatures::makeTxDbFromGFF(gff3_fn, format = "gff3")
 
 
     genes <- genes(txdb) %>% sort
